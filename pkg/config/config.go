80efad174fa25af3fdf3c7bdd0b34615de70aa43
Bjørn Åge Tungesvik <BjornAge.Tungesvik@skatteetaten.no>
2020-03-06 10:38:54 +0100

    SITJ-686 fix typo

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -482,7 +482,7 @@ func findOutputRepository(dockerName string) (string, error) {
 	//name, err := reference.ParseNamed(dockerName)
 	if err != nil {
 		return "", errors.Wrap(err, "Error parsing docker registry reference")
-	
+	}
 
 	return reference.Path(name), nil
 
